cdb944aeeecfaa3fd3f1e4fbf203df02f4de138a
samsepi63194 <209249477+samsepi63194@users.noreply.github.com>
2026-03-08 14:06:37 +0100

    change name file

---
+++ .config/fish/config.fish
@@ -0,0 +1,19 @@
+if status is-interactive
+# Commands to run in interactive sessions can go here
+end
+# By knightfall01 on Github as a part of https://github.com/Knightfall01/Hyprland-config.
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+starship init fish | source
+set -U fish_greeting ""
+
+starship init fish | source
+fastfetch
+# Theme
+
+# Created by `pipx` on 2026-02-24 10:30:09
+set PATH $PATH /home/lain/.local/bin
+
+fish_add_path /home/lain/.spicetify
